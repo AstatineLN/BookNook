79fe07f04f4840ad2e6dda00c59f6023ff1786bd
Jatzy Saenz <jatzirysaenzpardo@outlook.com>
2024-11-16 00:45:44 -0600

    labels and additional requests file

--- BookNook.java
+++ BookNook.java
@@ -1,8 +1,80 @@
 
+
+class Library
+{
+    //attributes
+    
+
+    //getters
+
+    //setters
+
+    //constructors(not sure if needed)
+
+    //methods
+}
+class Book
+{
+    //attributes
+
+    //getters
+
+    //setters
+
+    //constructors
+
+    //methods
+}
+class User
+{
+    //attributes
+
+    //getters
+
+    //setters
+
+    //constructors
+
+    //methods
+}
+ class LibraryOwner extends User 
+{
+    //attributes
+
+    //getters
+
+    //setters
+
+    //constructors
+
+    //methods
+
+}
+class Request
+{
+    //attributes
+
+    //getters
+
+    //setters
+
+    //constructors
+
+    //methods
+
+}
+
 public class BookNook 
 {
     public static void main(String args[])
     {
+        //first option screen
 
+        //option screen to make account
+
+        //option screen for basic user
+
+        //option screen for library owner
+        
     }
 }
